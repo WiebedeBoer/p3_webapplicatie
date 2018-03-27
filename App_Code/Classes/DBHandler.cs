52e5b9b4ec167e6eddfc27aff2d232617c4a2ff7
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-03-27 12:58:25 +0200

    Add files via upload

---
+++ App_Code/Classes/DBHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.Data;
+
+/// <summary>
+/// Summary description for DBHandler
+/// </summary>
+public class DBHandler
+{
+    private Database db;
+    public DBHandler()
+    {
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\whiteboard.mdf;Integrated Security=True";
+
+        string provider = "System.Data.SqlClient";
+
+        db = Database.OpenConnectionString(connectionString, provider);
+    }
+    public Database getConnection()
+    {
+        return db;
+    }
+}
