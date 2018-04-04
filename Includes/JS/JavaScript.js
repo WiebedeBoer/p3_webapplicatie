fc19ce8fe5b0b55b9fd4324c702b4790605c23ec
Redmar <32055364+Redmar555@users.noreply.github.com>
2018-04-04 16:44:23 +0200

    Add files via upload

---
+++ Includes/JS/JavaScript.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $(".linkSub").click(function () {
+        $(".iframeDat").attr("src", $(".linkTxt").val());
+    });
+
+
+    $(".iframeWidth").keyup(function () {
+        if ($(".iframeWidth").val() < 1500) {
+            $(".iframeWrapper").css("width", $(".iframeWidth").val() + "px");
+        } else {
+            $(".iframeWrapper").css("width", "1500px");
+        }
+    });
+
+
+
+    $(".iframeHeight").keyup(function () {
+        if ($(".iframeHeight").val() < 1000) {
+            $(".iframeWrapper").css("height", $(".iframeHeight").val() + "px");
+        } else {
+            $(".iframeWrapper").css("height", "1000px");
+        }
+        
+    });
+});
